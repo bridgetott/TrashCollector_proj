186c0d21c757dbf1ea888e204bcc2c497f42801c
unknown <bridgetott@yahoo.com>
2020-10-23 14:40:11 -0500

    Routing by creating a custom action filt and Authorization filter attribute

---
+++ TrashCollectorProj/TrashCollectorProj/Models/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorProj.Models
+{
+    [Authorize(Roles = "Customer")]
+    public class CustomersController : Controller
+    {
+    }
+}
